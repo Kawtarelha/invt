fb9682890d0afe835d1d1a4dfdc1c634db2b0e76
Kawtarelha <111359605+Kawtarelha@users.noreply.github.com>
2022-09-07 11:58:01 +0200

    Add files via upload

---
+++ bienvenu.php
@@ -0,0 +1,29 @@
+<?php
+session_start() ;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="bienvenu.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <title>Document</title>
+</head>
+<body>
+ 
+    <div class="menu">
+        <h1 class="logo">CIE-<span>AUTOMOTIVE</span></h1>
+        <ul>
+            <li><a href="Client.php">Clients</a></li>
+            <li><a href="#">Fiche Reference</a></li>
+            <li><a href="Stock.php">Image du Stock</a></li>
+            <li><a href="#">Zones</a></li>
+         </ul>
+    </div>
+    
+
+  
+</body>  
+</html>
